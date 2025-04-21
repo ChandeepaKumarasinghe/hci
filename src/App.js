6b5a25f4204c2714efaa99ced7e119c463cb40c2
KavindaKiridana <kavindakiridena@gmail.com>
2025-04-21 15:38:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import RoomDesigner from './components/RoomDesigner';
+import Login from './components/Login';
+import Register from './components/Register';
 import './styles.css';
 
 function App() {
