38c559dcb40b162e07c74bae6d8f6f57af7fc3c3
KavindaKiridana <kavindakiridena@gmail.com>
2025-04-23 20:55:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,6 @@ function App() {
               <Route path="/payment" element={<Payment />} />
               <Route path="/admin" element={<AdminDashboard />} />
               <Route path="/designer" element={<RoomDesigner />} />
-
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
             </Routes>
